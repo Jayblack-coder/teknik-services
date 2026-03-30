578e9c4ac9d19de91494736613d1fcf14aa3f33a
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2026-03-30 10:34:36 +0100

    user

--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
 // REGISTER
@@ -9,13 +9,12 @@ exports.register = async (req, res) => {
      console.log("BODY:", req.body);
     const { name, email, password, role } = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    
 
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       role
     });
 
