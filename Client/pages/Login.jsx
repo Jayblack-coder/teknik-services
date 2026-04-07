8e937e1e294bcb2096b0006a70860a9cf42dc2e6
Ejike Chukwuma <ejikechukwuma056@gmail.com>
2026-04-07 15:56:24 +0100

    login

--- Client/pages/Login.jsx
+++ Client/pages/Login.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
-import axios from "axios";
+import { TextField, Button, Container } from "@mui/material";
+import API from "../utils/api";
 
 export default function Login() {
   const [form, setForm] = useState({ email: "", password: "" });
 
   const handleSubmit = async () => {
-    const res = await axios.post("http://localhost:3000/api/auth/login", form);
-    // 
-    localStorage.setItem("user", JSON.stringify(decodedUser));
-    alert("Logged in!");
+    const res = await API.post("/auth/login", form);
+
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+
+    window.location.href = "/";
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input placeholder="Email" onChange={e => setForm({...form, email: e.target.value})} />
-      <input placeholder="Password" type="password" onChange={e => setForm({...form, password: e.target.value})} />
-      <button onClick={handleSubmit}>Login</button>
-    </div>
+    <Container maxWidth="sm">
+      <TextField
+        fullWidth label="Email" margin="normal"
+        onChange={(e) => setForm({...form, email: e.target.value})}
+      />
+
+      <TextField
+        fullWidth label="Password" type="password" margin="normal"
+        onChange={(e) => setForm({...form, password: e.target.value})}
+      />
+
+      <Button fullWidth variant="contained" onClick={handleSubmit}>
+        Login
+      </Button>
+    </Container>
   );
 }
